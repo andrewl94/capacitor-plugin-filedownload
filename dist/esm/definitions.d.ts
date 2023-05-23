b13a006e21916b9061ca67a7c34e7b75a3c8b211
Andrew Luccas <andrewluccas@marcasite.com.br>
2023-05-23 11:41:14 -0300

    Dist commit

---
+++ dist/esm/definitions.d.ts
@@ -0,0 +1,23 @@
+import type { PluginListenerHandle } from '@capacitor/core';
+export interface FileDownloadPlugin {
+    download(options: FileDownloadOptions): Promise<FileDownloadResponse>;
+    addListener(eventName: 'downloadProgress', listenerFunc: (progress: FileDownloadProgress) => void): Promise<PluginListenerHandle> & PluginListenerHandle;
+}
+export interface FileDownloadOptions {
+    uri: string;
+    fileName: string;
+    /**
+     * Downloader Title， Only Android
+     */
+    downloadTitle?: string;
+    /**
+     * Downloader Description， Only Android
+     */
+    downloadDescription?: string;
+}
+export interface FileDownloadResponse {
+    path: string;
+}
+export interface FileDownloadProgress {
+    progress: number;
+}
